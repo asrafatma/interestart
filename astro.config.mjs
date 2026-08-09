881988f28d4c6c640586316c43d30b101385181e
asrafatma <waziralamasra@gmail.com>
2026-08-09 22:49:55 +0500

    Update site URL in configuration from example to production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://interestart.example',
+  site: 'https://interestart.com',
   integrations: [sitemap()],
   output: 'static'
 });
